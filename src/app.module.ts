01571d4230be723854dd2ae7a143cf522d7c5161
Nelson Botero <nelsonkennedy2004@gmail.com>
2026-03-15 09:09:54 -0500

    nuevo cambio produccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,3 +26,31 @@ import { ScheduleModule } from '@nestjs/schedule';
   ],
 })
 export class AppModule {}
+
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { WhatsappModule } from './whatsapp/whatsapp.module';
+// import { BulkModule } from './bulk/bulk.module';
+// import { AuthModule } from './auth/auth.module';
+// import { ScheduleModule } from '@nestjs/schedule';
+
+// @Module({
+//   imports: [
+//     ScheduleModule.forRoot(),
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: 'localhost',
+//       port: 5432,
+//       username: 'user_admin',
+//       password: 'my_password123',
+//       database: 'mass_sender_db',
+//       autoLoadEntities: true, 
+//       synchronize: true, 
+//     }),
+//     WhatsappModule,
+//     BulkModule,
+//     AuthModule
+//   ],
+// })
+// export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -32,3 +32,29 @@ async function bootstrap() {
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
+
+
+// main.ts (Backend)
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { join } from 'path';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+//   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+//     prefix: '/uploads/',
+//   });
+
+//   // Habilitar CORS
+//   app.enableCors({
+//     origin: ['http://localhost:3000', 'http://localhost:3001'], // Las URLs de tu Next.js
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+
+//   await app.listen(3000); // Asegúrate de que este sea el puerto que usa Axios en el front
+// }
+// bootstrap();
